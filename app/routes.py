771f2799646d92eacc4b154c89f9811cf605af43
BrisaAB <brisantuna@gmail.com>
2025-08-08 20:20:14 -0300

    Fonts y catalogo html
    Agregue las fonts de montserrat, cree el archivo html para el catalogo e hice las modificaciones en routes.py e index para que funcione

--- app/routes.py
+++ app/routes.py
@@ -5,3 +5,7 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/catalogo')
+def catalogo():
+    return render_template('catalogo.html')
